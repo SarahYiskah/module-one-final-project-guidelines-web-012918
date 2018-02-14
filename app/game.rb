30cbb69a3efbc88e7c43b34730d2ac8e81219b9b
gilsunshine <gilsunshine@gmail.com>
2018-02-14 15:18:14 -0500

    created README

--- app/game.rb
+++ app/game.rb
@@ -16,18 +16,28 @@ class Game < ActiveRecord::Base
 
     cities = City.city_distances(user)
     difficulty = 0
+    turn_count = 1
 
     10.times do
       turn = Turn.new(difficulty)
       sample_cities = City.get_cities(turn, cities)
       turn.display_cities(sample_cities)
       answer = turn.get_answer
+      answer_return = turn.check_answer(answer, sample_cities)
+      if turn_count != 10
+        if answer_return == 1
+          pid = fork{ exec 'afplay', "media/01 Winning Sound.mp3"}
+        elsif answer_return == 0
+          pid = fork{ exec 'afplay', "media/02 Losing Sound.mp3"}
+        end
+      end
       if answer == "exit" || answer == "quit"
         break
       else
-        game_score += turn.check_answer(answer, sample_cities)
+        game_score += answer_return
         difficulty +=1
       end
+      turn_count += 1
     end
 
     game = Game.create(score: game_score)
